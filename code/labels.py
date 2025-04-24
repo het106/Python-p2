be2b1f45fc467712dc660581bd72199aad001a74
Jake Telford <jkct1@st-andrews.ac.uk>
2025-04-24 16:18:11 +0100

    Added labels to own file, implemented use of it with the maps notebook

---
+++ code/labels.py
@@ -0,0 +1,165 @@
+social_grade_labels = {
+    -8.0: "Does not apply",
+     1.0: "AB Higher and intermediate managerial/administrative/professional occupations",
+	 2.0: "C1 Supervisory, clerical and junior managerial/administrative/professional occupations",
+	 3.0: "C2 Skilled manual occupations",
+	 4.0: "DE Semi-skilled and unskilled manual occupations; unemployed and lowest grade" 
+} 
+
+country_of_birth_labels = {
+    -8.0: "Does not apply",
+     1.0: "United Kingdom",
+     2.0: "Other countries"
+}
+
+economic_activity_labels = {
+    -8.0: "Does not apply",
+	 1.0: "Economically active (excluding full-time students): In employment: Employee",
+	 2.0: "Economically active (excluding full-time students): In employment: Self-employed",
+	 3.0: "Economically active (excluding full-time students): Unemployed: Seeking work or waiting to start a job already obtained",
+	 4.0: "Economically active and a full-time student",
+	 5.0: "Economically inactive: Retired",
+	 6.0: "Economically inactive: Student",
+	 7.0: "Economically inactive: Looking after home or family",
+	 8.0: "Economically inactive: Long-term sick or disabled",
+	 9.0: "Economically inactive: Other"
+}
+
+ethinic_groups_labels = {
+    -8.0: "Does not apply",
+     1.0: "Asian, Asian British or Asian Welsh",
+	 2.0: "Black, Black British, Black Welsh, Caribbean or African",
+	 3.0: "Mixed or Multiple ethnic groups",
+	 4.0: "White",
+	 5.0: "Other ethnic group",
+}
+
+health_labels = {
+    -8.0: "Does not apply",
+     1.0: "Very good health",
+     2.0: "Good health",
+     3.0: "Fair health",
+     4.0: "Bad health",
+     5.0: "Very bad health"
+}
+
+family_labels = {
+    -8.0: "Does not apply",
+	 1.0: "One-person household",
+	 2.0: "Married or civil partnership couple household",
+	 3.0: "Cohabiting couple household",
+	 4.0: "Lone parent household",
+	 5.0: "Multi-person household",
+}
+
+hours_labels = {
+    -8.0: "Does not apply",
+    1.0: "15 hours or less",
+    2.0: "16 to 30",
+    3.0: "31 to 48",
+    4.0: "49 or more"
+}
+
+education_labels = {
+    -8.0: "Does not apply",
+     1.0: "Student",
+     2.0: "Not a student",
+}
+
+industry_labels = {
+    -8.0: "Does not apply",
+    1.0: "A Agriculture, forestry and fishing",
+	2.0: "C Manufacturing",
+	3.0: "B, D, E Energy and water",
+	4.0: "F Construction",
+	5.0: "G, I Distribution, hotels and restaurants",
+	6.0: "H, J Transport and communication",
+	7.0: "K, L, M, N Financial, real estate, professional and administrative activities",
+	8.0: "O, P, Q Public administration, education and health",
+	9.0: "R, S, T, U Other"
+}
+
+london_labels = {
+    -8.0: "Does not apply",
+    "E13000001": "Inner London",
+    "E13000002": "Outer London"
+}
+
+partner_labels = {
+    -8.0: "Does not apply",
+     1.0: "Never married and never registered a civil partnership",
+     2.0: "Married or in a registered civil partnership",
+     3.0: "Separated, but still legally married or still legally in a civil partnership",
+     4.0: "Divorced or civil partnership dissolved",
+     5.0: "Widowed or surviving civil partnership partner"
+}
+
+occupation_labels = {
+    -8.0: "Does not apply",
+	 1.0: "Managers, directors and senior officials",
+	 2.0: "Professional occupations",
+	 3.0: "Associate professional and technical occupations",
+	 4.0: "Administrative and secretarial occupations",
+	 5.0: "Skilled trades occupations",
+	 6.0: "Caring, leisure and other service occupations",
+	 7.0: "Sales and customer service occupations",
+	 8.0: "Process, plant and machine operatives",
+	 9.0: "Elementary occupations"
+}
+
+region_labels = {
+    "E12000001":  "North East",
+	"E12000002":  "North West",
+	"E12000003":  "Yorkshire and The Humber",
+	"E12000004":  "East Midlands",
+	"E12000005":  "West Midlands",
+	"E12000006":  "East of England",
+	"E12000007":  "London",
+	"E12000008":  "South East",
+	"E12000009":  "South West",
+	"W92000004":  "Wales",
+	"N99999999":  "Does not apply: Northern Ireland",
+	"S99999999":  "Does not apply: Scotland"
+}
+
+religion_labels = {
+    -8.0: "Does not apply",
+	 1.0: "No religion",
+	 2.0: "Christian",
+	 3.0: "Buddhist",
+	 4.0: "Hindu",
+	 5.0: "Jewish",
+	 6.0: "Muslim",
+	 7.0: "Sikh",
+	 8.0: "Other religion",
+	 9.0: "Not answered"
+}
+
+residence_labels = {
+    1.0: "Lives in a household",
+    2.0: "Lives in a communal establishment"
+}
+
+age_labels = {
+    -8.0: "Does not apply",
+     1.0: "Aged 15 years and under",
+     2.0: "Aged 16 to 24 years",
+     3.0: "Aged 25 to 34 years",
+     4.0: "Aged 35 to 44 years",
+     5.0: "Aged 45 to 54 years",
+     6.0: "Aged 55 to 64 years",
+     7.0: "Aged 65 years and over"
+}
+
+sex_labels = {
+    -8.0: "Does not apply",
+     1.0: "Female",
+     2.0: "Male"
+}
+
+usual_student_labels = {
+    -8.0: "Does not apply",
+     1.0: "Is a usual resident",
+     2.0: "Is a student living at an alternative address in term time",
+     3.0: "Is a non-UK-born short-term resident, staying 3 to 12 months"
+}
